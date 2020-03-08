b85e14c2293d8a0a83fe214a6a852d523236f8b2
eugenioclrc <eugenioclrc@gmail.com>
2020-03-08 20:16:52 -0300

    phaser

--- README.md
+++ README.md
@@ -1,22 +1,46 @@
-# square-puzzle
+Phaser3 Es6 Webpack4 Boilerplate
+===========
 
-> Simple puzzle game
+> Plain webpack 4 Boilerplate with Babel, SASS and Phaser3 on board
 
-## Build Setup
+## Setup
+You'll need to install a few things before you have a working copy of the project.
 
-``` bash
-# install dependencies
-$ npm install
+### 1. Clone this repo:
+Navigate into your workspace directory.
 
-# serve with hot reload at localhost:3000
-$ npm run dev
+Run:
+```sh
+$ git clone https://github.com/rafaeldelboni/phaser3-es6-webpack4.git
+```
+### 2. Install node.js and [yarn](https://yarnpkg.com/):
+https://nodejs.org/en/
 
-# build for production and launch server
-$ npm run build
-$ npm run start
+### 3. Install dependencies:
+Navigate to the cloned repo's directory.
 
-# generate static project
-$ npm run generate
+Run:
+```sh
+$ yarn
 ```
 
-For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+## Development
+Run the local webpack-dev-server with livereload and autocompile on [http://localhost:3000/](http://localhost:3000/)
+```sh
+$ yarn dev
+```
+## Deployment
+Build the current application
+```sh
+$ yarn build
+```
+
+## [webpack](https://webpack.js.org/)
+If you're not familiar with webpack, the [webpack-dev-server](https://webpack.js.org/configuration/dev-server/) will serve the static files in your build folder and watch your source files for changes.
+When changes are made the bundle will be recompiled. This modified bundle is served from memory at the relative path specified in publicPath.
+
+## Credits
+
+Phaser + ES6: https://github.com/lean/phaser-es6-webpack  
+Webpack 4 + Babel: https://github.com/cvgellhorn/webpack-boilerplate  
+Phaser 3 + ES6 + Webpack 3: https://github.com/nkholski/phaser3-es6-webpack  
---
+++ src/scenes/Game.js
@@ -0,0 +1,154 @@
+import Phaser from 'phaser'
+
+let player
+
+const map = `
+111
+101
+111
+111
+109
+`.trim().split(`\n`).map(e => e.trim().split('').map(e => parseInt(e, 10)))
+
+const validMoves = map.map(r => r.map(e => e === 1))
+console.log(validMoves)
+const players = []
+const gridSize = 64
+window.players = players
+function calculateMoves (player) {
+  player.validMoves = {
+    left: (validMoves[player.y] && validMoves[player.y][player.x - 1]) || false,
+    right: (validMoves[player.y] && validMoves[player.y][player.x + 1]) || false,
+    up: (validMoves[player.y - 1] && validMoves[player.y - 1][player.x]) || false,
+    down: (validMoves[player.y + 1] && validMoves[player.y + 1][player.x]) || false
+  }
+}
+
+export default class extends Phaser.Scene {
+  constructor () {
+    super({ key: 'Game' })
+  }
+
+  eat (j, i) {
+    console.log(j, i)
+    validMoves[i][j] = false
+    window.group.add(this.add.rectangle(gridSize * j, gridSize * i, gridSize, gridSize, 0xff0000).setOrigin(0.5))
+  }
+
+  create () {
+    this.animationsEnd = true
+    window.group = this.add.container()
+    // window.group.setOrigin(0.5, 0.5)
+    // window.group.setX(100)
+    let r
+
+    for (let i = 0; i < map.length; i += 1) {
+      console.log(i)
+      for (let j = 0; j < map[0].length; j += 1) {
+        if (map[i][j] > 0) {
+          r = this.add.rectangle(gridSize * j, gridSize * i, gridSize, gridSize, 0x6666ff).setOrigin(0.5)
+          // r.setStrokeStyle(1, 0xffff00, 0.5)
+          window.group.add(r)
+        }
+        if (map[i][j] === 2 || map[i][j] > 5) {
+          // pintado
+          this.eat(j, i)
+        }
+        if (map[i][j] > 5) {
+          player = this.add.container()
+          window.p = player
+
+          window.rect = this.add.rectangle(gridSize * j, gridSize * i, gridSize, gridSize, 0xff0000).setOrigin(0.5)
+          player.add(window.rect)
+          window.circle = this.add.circle(gridSize * j, gridSize * i, gridSize * 0.2, 0xffff00).setOrigin(0.5)
+          player.add(window.circle)
+          window.group.add(player)
+          players.push({ player, x: j, y: i })
+          console.log(calculateMoves(players[0]))
+        }
+      }
+    }
+    window.group.setX(parseInt(window.screen.width - (window.group.getBounds().width)) / 2)
+    window.group.setY(parseInt(window.screen.height - (window.group.getBounds().height)) / 2)
+
+    /*
+    // window.group.add(this.add.rectangle(0, 0, 0, 40, 0x6666ff).setOrigin(0))
+    // window.group.add(this.add.rectangle(0, 0, 40, 40, 0x6666ff).setOrigin(0))
+
+    window.g = this.add.grid(screen.width / 2, screen.height / 2, gridWidth * gridSize, gridHeight * gridSize, gridSize, gridSize, 0x999999, 1, 0x666666).setOrigin(0.5, 0.5)
+
+    this.logo = new Logo(this, 400, 150)
+    this.tweens.add({
+      targets: this.logo,
+      y: 450,
+      duration: 2000,
+      ease: 'Power2',
+      yoyo: true,
+      loop: -1
+    })
+
+    this.mushroom = new Mushroom(this, 0, 0)
+    Phaser.Display.Align.In.Center(this.mushroom, this.add.zone(400, 300, 800, 600))
+    */
+  }
+
+  move (direction) {
+    const onComplete = () => {
+      this.animationsEnd = true
+    }
+    players.forEach(p => {
+      if (!p.validMoves[direction]) {
+        return
+      }
+      this.animationsEnd = false
+      let x = p.player.x
+      let y = p.player.y
+
+      const mul = {
+        up: -1,
+        down: +1,
+        left: -1,
+        right: +1
+      }
+      this.eat(p.x, p.y)
+      if (['up', 'down'].includes(direction)) {
+        y = p.player.y + (mul[direction] * gridSize)
+        p.y = p.y + mul[direction]
+      } else {
+        x = p.player.x + (mul[direction] * gridSize)
+        p.x = p.x + mul[direction]
+      }
+
+      this.tweens.add({
+        targets: p.player,
+        x,
+        y,
+        onComplete,
+        duration: 300,
+        ease: 'Power2'
+      })
+      calculateMoves(p)
+      console.log(p.validMoves)
+    })
+  }
+
+  update () {
+    if (!this.animationsEnd) {
+      return
+    }
+
+    // this.mushroom.update()
+    const cursors = this.input.keyboard.createCursorKeys()
+    if (cursors.up.isDown) {
+      this.move('up')
+    } else if (cursors.down.isDown) {
+      this.move('down')
+    } else if (cursors.right.isDown) {
+      this.move('right')
+    } else if (cursors.left.isDown) {
+      this.move('left')
+    } else {
+      // reset anims
+    }
+  }
+}
